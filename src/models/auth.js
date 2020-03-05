589d5c35f367ca27357ff15b187419060d844d28
dany_arie <dany_arie@yahoo.com>
2020-03-05 23:01:22 +0700

    add auth model

---
+++ src/models/auth.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const AuthSchema = mongoose.Schema({
+  username: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+});
+
+// export model user with AuthSchema
+module.exports = mongoose.model("auth", AuthSchema);
